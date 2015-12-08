0490780ce05084612fb872afd967322abfd1b297
eryshev <a.a.eryshev@gmail.com>
2015-12-08 22:31:02 +0100

    Add phaser first game assets

--- client/index.js
+++ client/index.js
@@ -1,4 +1,18 @@
-import PIXI from 'pixi.js';
-import p2 from 'p2';
+import * as Phaser from "Phaser";
 
-console.log('Hello world!');
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+    game.load.image('sky', 'assets/sky.png');
+    game.load.image('ground', 'assets/platform.png');
+    game.load.image('star', 'assets/star.png');
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+
+    game.add.sprite(0, 0, 'sky');
+}
+
+function create() {
+}
+
+function update() {
+}
