2d6f1b2cdefd3f145327aec7af6446ace3f0e898
eryshev <a.a.eryshev@gmail.com>
2015-12-12 17:17:50 +0100

    Add dude and platforms from Phaser tuto

--- client/index.js
+++ client/index.js
@@ -11,6 +11,8 @@ var PIXI = window.PIXI;
 var P2 = window.p2;
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var player;
+var platforms;
 
 function preload() {
     game.load.image('sky', 'sky.png');
@@ -20,8 +22,32 @@ function preload() {
 }
 
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //background
     game.add.sprite(0, 0, 'sky');
+
+    //platforms
+    platforms = game.add.group();
+    platforms.enableBody = true;
+    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    ground.scale.setTo(2,2);
+    ground.body.immovable = true;
+    var ledge = platforms.create(400, 400, 'ground');
+    ledge.body.immovable = true;
+    ledge = platforms.create(-150, 250, 'ground');
+    ledge.body.immovable = true;
+
+    //player
+    player = game.add.sprite(32, game.world.height - 150, 'dude');
+    game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.5;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
 }
 
 function update() {
+    game.physics.arcade.collide(player, platforms);
 }
