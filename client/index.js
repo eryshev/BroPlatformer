c88169a83078066abd3b711896dc956d2d15dca2
eryshev <a.a.eryshev@gmail.com>
2015-12-12 20:42:45 +0100

    Add star collect et score

--- client/index.js
+++ client/index.js
@@ -6,13 +6,26 @@
 
 }(window));
 
+//engine
 var Phaser = window.phaser;
 var PIXI = window.PIXI;
 var P2 = window.p2;
 
+//assets
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var player;
 var platforms;
+var cursors;
+var stars;
+var score = 0;
+var scoreText;
+
+function collectStar(player, star) {
+    star.kill();
+
+    score += 10;
+    scoreText.text = 'score: ' + score;
+}
 
 function preload() {
     game.load.image('sky', 'sky.png');
@@ -46,8 +59,40 @@ function create() {
     player.animations.add('left', [0, 1, 2, 3], 10, true);
     player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+    //stars
+    stars = game.add.group();
+    stars.enableBody = true;
+
+    for (var i = 0; i < 12; ++i) {
+        var star = stars.create(i* 70, 0, 'star');
+        star.body.gravity.y = 150;
+        star.body.bounce.y = 0.6 + Math.random() * 0.4;
+    }
+
+    //score
+    scoreText = game.add.text(16, 16, 'score: ' + score, { fontSize: '32px', fill: '#000' });
 }
 
 function update() {
     game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    player.body.velocity.x = 0;
+    if (cursors.left.isDown) {
+        player.body.velocity.x = -150;
+        player.animations.play('left');
+    } else if (cursors.right.isDown) {
+        player.body.velocity.x = 150;
+        player.animations.play('right');
+    } else {
+        player.animations.stop();
+        player.frame = 4;
+    }
+
+    if (cursors.up.isDown && player.body.touching.down) {
+        player.body.velocity.y = -350;
+    }
 }
